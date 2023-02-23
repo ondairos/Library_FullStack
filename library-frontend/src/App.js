996d5dccabc6339ee20ca7a189a8bb928357cec7
Ondairos <ondairos@gmail.com>
2023-02-23 17:31:53 +0200

    refactored server with books/authors

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -3,8 +3,30 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client'
+
+
+
+const ALL_BOOKS = gql`
+query {
+    allBooks {
+        title,
+        published,
+        author,
+        id
+    }
+}
+`
+
+
+
 const App = () => {
   const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_BOOKS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -12,6 +34,9 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <div>
+          {/* {result.data.allBooks.map(book => book.title).join(', ')} */}
+        </div>
       </div>
 
       <Authors show={page === 'authors'} />
