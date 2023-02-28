9ffaefe6f8cb008da6d9d13296db2331baeeb738
Ondairos <ondairos@gmail.com>
2023-02-28 12:58:15 +0200

    added model files

---
+++ graphql-library-backend/models/books.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+
+const schema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    published: {
+        type: String,
+        required: false,
+        minlength: 3
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+})
+
+module.exports = mongoose.model('Book', schema)
