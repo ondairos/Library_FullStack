976697dcea60cd82159b6d406147c4f0b9ed981e
Ondairos <ondairos@gmail.com>
2023-03-07 14:06:56 +0200

    modified config in index.js for websocket usage

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -5,13 +5,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { setContext } from '@apollo/client/link/context'
 
 import {
-    ApolloClient, InMemoryCache, ApolloProvider, createHttpLink
+    ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, split
 } from '@apollo/client'
 
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000',
 })
+
+//websocket link
+const wsLink = new GraphQLWsLink(
+    createClient({ url: 'ws://localhost:4000' })
+)
 
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem('booksandauthors-user-token')
@@ -23,10 +31,20 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+            definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    authLink.concat(httpLink)
+)
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(httpLink)
+    link: splitLink
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
